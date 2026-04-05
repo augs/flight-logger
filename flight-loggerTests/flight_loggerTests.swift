24a8a0bb27ed64706195d933d6118973597b6f1b
august huber <gus@pbx.org>
2026-04-04 22:26:23 -0400

    Initial Commit

---
+++ flight-loggerTests/flight_loggerTests.swift
@@ -0,0 +1,18 @@
+//
+//  flight_loggerTests.swift
+//  flight-loggerTests
+//
+//  Created by august huber on 4/4/26.
+//
+
+import Testing
+
+struct flight_loggerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
